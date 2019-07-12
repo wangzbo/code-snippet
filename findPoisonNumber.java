b79bf7f9f746abd4566f5add4e30513db7463792
wangzbo <42736964+wangzbo@users.noreply.github.com>
2019-07-12 14:34:30 +0800

    Update findPoisonNumber.java

--- findPoisonNumber.java
+++ findPoisonNumber.java
@@ -13,59 +13,52 @@
 private HashMap<Integer, ArrayList<Integer>> mousePoisonMap = new HashMap<Integer, ArrayList<Integer>>();
 
 public void buildMousePoisonMap() {
-		int [][] poisonArray = {{1,2,3},{4,5,6},{7,8,9}};
-		ArrayList<ArrayList<Integer>> poisonMouseList = new ArrayList<ArrayList<Integer>>();
-		for(int i = 0; i < poisonArray.length * poisonArray[0].length; i++) {
-			poisonMouseList.add(new ArrayList<Integer>());
+	int[][] poisonArray = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+	ArrayList<ArrayList<Integer>> poisonMouseList = new ArrayList<ArrayList<Integer>>();
+	for (int i = 0; i < poisonArray.length * poisonArray[0].length; i++) {
+		poisonMouseList.add(new ArrayList<Integer>());
+	}
+	for (int i = 0; i < poisonArray.length; i++) {
+		for (int j = 0; j < poisonArray[0].length; j++) {
+			poisonMouseList.get(poisonArray[i][j] - 1).add(i + 1);
+			poisonMouseList.get(poisonArray[i][j] - 1).add(j + poisonArray.length + 1);
 		}
-		int mouse_index = 1;
-		for(int i = 0; i < poisonArray.length; i++) {
-			for(int j = 0; j < poisonArray[0].length; j++) {
-				poisonMouseList.get(poisonArray[i][j]-1).add(mouse_index);
-			}
-			mouse_index++;
+	}
+	for (int i = 0; i < poisonArray.length; i++) {
+		poisonMouseList.get(poisonArray[i][i] - 1).add(poisonArray.length + poisonArray[0].length + 1);
+	}
+
+	for (int i = 0; i < poisonMouseList.size(); i++) {
+		int sum = 0;
+		for (Integer itg : poisonMouseList.get(i)) {
+			sum += (1 << (itg.intValue() - 1));
 		}
-		for(int j = 0; j < poisonArray[0].length; j++) {
-			for(int i = 0; i < poisonArray.length; i++) {
-				poisonMouseList.get(poisonArray[i][j]-1).add(mouse_index);
-			}
-			mouse_index++;
-		}
-		for(int i = 0; i < poisonArray.length; i++) {
-			poisonMouseList.get(poisonArray[i][i]-1).add(mouse_index);
-		}
-		
-		for(int i = 0; i < poisonMouseList.size(); i++) {
+		ArrayList<Integer> poisonCombine = new ArrayList<Integer>();
+		poisonCombine.add(i + 1);
+		poisonCombine.add(poisonArray.length * poisonArray[0].length + 1);
+		mousePoisonMap.put(sum, poisonCombine);
+	}
+	for (int i = 0; i < poisonMouseList.size() - 1; i++) {
+		for (int j = i + 1; j < poisonMouseList.size(); j++) {
+			HashSet<Integer> mouseSet = new HashSet<Integer>();
+			mouseSet.addAll(poisonMouseList.get(i));
+			mouseSet.addAll(poisonMouseList.get(j));
 			int sum = 0;
-			for(Integer itg : poisonMouseList.get(i)) {
+			for (Integer itg : mouseSet) {
 				sum += (1 << (itg.intValue() - 1));
 			}
 			ArrayList<Integer> poisonCombine = new ArrayList<Integer>();
-			poisonCombine.add(i+1);
-			poisonCombine.add(10);
-			mousePoisonMap.put(sum,poisonCombine);
-		}
-		for(int i = 0; i < poisonMouseList.size()-1; i++) {
-			for(int j = i+1; j < poisonMouseList.size(); j++) {
-				HashSet<Integer> mouseSet = new HashSet<Integer>();
-				mouseSet.addAll(poisonMouseList.get(i));
-				mouseSet.addAll(poisonMouseList.get(j));
-				int sum = 0;
-				for(Integer itg : mouseSet) {
-					sum += (1 << (itg.intValue() - 1));
-				}
-				ArrayList<Integer> poisonCombine = new ArrayList<Integer>();
-				poisonCombine.add(i+1);
-				poisonCombine.add(j+1);
-				mousePoisonMap.put(sum,poisonCombine);
-			}
+			poisonCombine.add(i + 1);
+			poisonCombine.add(j + 1);
+			mousePoisonMap.put(sum, poisonCombine);
 		}
 	}
+}
 	
-	public ArrayList<Integer> getPoisonNum(ArrayList<Integer> deadMouse){
-		int sum = 0;
-		for(Integer itg : deadMouse) {
-			sum += (1 << (itg.intValue() - 1));
-		}
-		return mousePoisonMap.get(sum);
+public ArrayList<Integer> getPoisonNum(ArrayList<Integer> deadMouse){
+	int sum = 0;
+	for(Integer itg : deadMouse) {
+		sum += (1 << (itg.intValue() - 1));
 	}
+	return mousePoisonMap.get(sum);
+}
