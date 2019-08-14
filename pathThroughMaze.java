6cdbe580e82b607e971b5c732ee021f8e21c5637
wangzbo <42736964+wangzbo@users.noreply.github.com>
2019-08-14 23:30:32 +0800

    Update pathThroughMaze.java

--- pathThroughMaze.java
+++ pathThroughMaze.java
@@ -24,7 +24,8 @@
     }
     
     public static void main(String args[]) {
-            //  0-0 0 1 0 1
+        
+        //  0-0 0 1 0 1
 		    //    |
 		    //  1 0 1 0-0-0
 		    //    |   |   |
